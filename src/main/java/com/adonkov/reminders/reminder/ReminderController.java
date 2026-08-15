12e48f2877bcfedec64fd1bd600d1df3b255f124
adonkovvv <81181363+adonkovvv@users.noreply.github.com>
2026-08-15 15:30:59 +0300

    crud endpoints

---
+++ src/main/java/com/adonkov/reminders/reminder/ReminderController.java
@@ -0,0 +1,52 @@
+package com.adonkov.reminders.reminder;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reminders")
+public class ReminderController {
+
+    private final ReminderService service;
+
+    public ReminderController(ReminderService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<ReminderDtos.Response> list() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ReminderDtos.Response get(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ReminderDtos.Response create(@RequestBody ReminderDtos.CreateRequest request) {
+        return service.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public ReminderDtos.Response update(@PathVariable Long id, @RequestBody ReminderDtos.UpdateRequest request) {
+        return service.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
